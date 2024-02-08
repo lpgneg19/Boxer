a170365fb72af210709e31022d38cd38ec1ab484
C.W. Betts <computers57@hotmail.com>
2024-02-07 23:57:50 -0700

    Oops, fix the bundled app.

--- Standalone/Boxer Standalone-Bridging-Header.h
+++ Standalone/Boxer Standalone-Bridging-Header.h
@@ -7,7 +7,6 @@
 #import "NSError+ADBErrorHelpers.h"
 #import "RegexKitLite.h" //FIXME: we shouldn't introduce dependencies on 3rd-party libraries.
 #import "NSShadow+ADBShadowExtensions.h"
-#import "NSImage+ADBImageEffects.h"
 #import "NSBezierPath+MCAdditions.h"
 #import "ADBGeometry.h"
 #import "BXMIDIDevice.h"
